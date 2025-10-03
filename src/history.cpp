022ae7b3e3f96395939cc4ac55baa665117ee117
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-10-03 13:09:38 +0200

    Bench: 8726965

--- src/history.cpp
+++ src/history.cpp
@@ -15,6 +15,9 @@ i32 History::get_conthist(const Position& pos, Move move, i32 ply, Search::Stack
     if (ply >= 2 && (ss - 2)->cont_hist_entry != nullptr) {
         stats += (*(ss - 2)->cont_hist_entry)[stm_idx][pt_idx][move.to().raw];
     }
+    if (ply >= 3 && (ss - 3)->cont_hist_entry != nullptr) {
+        stats += (*(ss - 3)->cont_hist_entry)[stm_idx][pt_idx][move.to().raw] / 2;
+    }
     if (ply >= 4 && (ss - 4)->cont_hist_entry != nullptr) {
         stats += (*(ss - 4)->cont_hist_entry)[stm_idx][pt_idx][move.to().raw];
     }
